fe191fefe55a435238f34c1dc8ff57587aec263c
LudwigLEDE <ludwig.engelhardt@gmail.com>
2024-10-01 20:39:18 +0200

    asdas

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -16,7 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-<<<<<<< Updated upstream
 // Email/Password Sign-in function
 export const signInWithEmailPassword = async (email, password) => {
   try {
